79ccace6d79e234f567ef9b3ee7a375484286c75
TheWolfAI Owner <owner@thewolfai.com>
2025-06-20 16:36:45 +1000

    cc

--- auto_contract_hunter.js
+++ auto_contract_hunter.js
@@ -1,40 +1,58 @@
---- a/;import React, { useState, useEffect } from 'react';
-+++ b/;import React, { useState, useEffect } from 'react';
-@@ -1,5 +1,4; @@
--;import React, { useState, useEffect, useRef, useCallback } from 'react';
--;import {
-+;import React, { useState, useEffect } from 'react';import {
-   Box,
-   Card,
-   CardContent,
-@@ -13,8 +11,8; @@
-   DialogTitle,
-   DialogContent,
-   DialogActions,
--  TextField,
-   List,
-+  TextField,
-   ListItem,
-   ListItemText,
-   ListItemSecondaryAction,
-@@ -23,10 +21,8; @@
-   AccordionDetails,
-   Alert,
-   Tabs,
--  Tab,
--  Divider,
--  Tooltip,
--  Badge,
-+  Tab, Tooltip,
-+  Badge, Divider,
-   Paper;
- } from; '@mui/material';
-+;import {
- import {
-   Code,
-   OpenInNew,
-@@ -113,3 +109,4; @@
-   )
- }
- 
+---a /;
+
++++b /;
+
+@@
+-1, 5 + 1, 4;
+@@
+-;
+
+-;
+import {
+
++;
+import {
+  Box,
+  Card,
+  CardContent
+
+@@
+-13, 8 + 11, 8;
+@@
+  DialogTitle,
+DialogContent,
+  DialogActions,
+  -TextField,
+  List,
+  +TextField,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+@@ - 23, 10 + 21, 8;
+@@
+  AccordionDetails,
+Alert,
+  Tabs,
+  -Tab,
+  -Divider,
+  -Tooltip,
+  -Badge,
+  +Tab, Tooltip,
+  +Badge, Divider,
+  Paper;
+}
+from;
+'@mui/material';
++;
+import {
+import {
+  Code,
+  OpenInNew
+
+@@
+-113, 3 + 109, 4;
+@@
+)
+}
+
 +;
